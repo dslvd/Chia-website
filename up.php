c3e45aaa40ed46831542c818f6af58d84269efee
dslvd <80934874+dslvd@users.noreply.github.com>
2021-10-25 11:26:08 +0800

    Update up.php

--- up.php
+++ up.php
@@ -1,6 +1,6 @@
 <?php
 $secret_key = "chiakey"; //Set this as your secret key, to prevent others uploading to your server.
-$sharexdir = "images/"; //This is your file dir, also the link..
+$sharexdir = "/assets/img/"; //This is your file dir, also the link..
 $domain_url = 'https://cdn.chiabot.xyz/'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
 
