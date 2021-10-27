f80d9c42abd08a8606c71ba812845334c6dd6ac1
dslvd <80934874+dslvd@users.noreply.github.com>
2021-10-27 21:18:35 +0800

    v2??

--- script.js
+++
@@ -1,9 +0,0 @@
-let counter = document.querySelector('h1');
-let count = 1;
-setInterval(()=>{
-    counter.innerText = count;
-    count++
-    
-    if(count > 5) location.replace('https://discord.gg/U4AzzphUEX')
-    
-},1000)
