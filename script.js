68d853acf8fe2d39341a28d427cfdbaf2b870f1a
dslvd <80934874+dslvd@users.noreply.github.com>
2021-10-28 15:45:26 +0800

    More faster redirect GOSDJHGSJKGHSKJSDG

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let counter = document.querySelector('h1');
-let count = 5;
+let count = 3;
 setInterval(()=>{
     counter.innerText = count;
     count++
     
     if(count > 1) location.replace('https://discord.gg/MKXzrb8wfN')
     
-},9000)
+},6000)
