d2d822cce129ec8eb05c40034309ecf8b4392399
dslvd <80934874+dslvd@users.noreply.github.com>
2021-10-28 13:58:07 +0800

    changes to thankyou.html

---
+++ script.js
@@ -0,0 +1,9 @@
+let counter = document.querySelector('h1');
+let count = 5;
+setInterval(()=>{
+    counter.innerText = count;
+    count++
+    
+    if(count > 1) location.replace('https://discord.gg/MKXzrb8wfN')
+    
+},10000)
