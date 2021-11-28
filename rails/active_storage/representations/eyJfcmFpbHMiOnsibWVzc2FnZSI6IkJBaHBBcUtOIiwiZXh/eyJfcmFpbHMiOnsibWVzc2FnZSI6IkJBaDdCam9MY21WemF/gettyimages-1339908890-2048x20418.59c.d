83fae8cc633b75f6ffc3cd1a0eaaabab66593845
dslvd <80934874+dslvd@users.noreply.github.com>
2021-11-28 12:26:11 +0800

    new again

---
+++ rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcUtOIiwiZXh/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9MY21WemF/gettyimages-1339908890-2048x20418.59c.d
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from konect.gg/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcUtOIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--1aaa6982f0beefd50ce53d4a6a6046bb83d3d9f6/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9MY21WemFYcGxTU0lNTkRBd2VEUXdNQVk2QmtWVSIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--ae7ea8630d18c3c324709f33325a060a2246c8b9/gettyimages-1339908890-2048x20418.jpg by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Nov 2021 03:22:51 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=https://params-gg-storage.s3.eu-west-1.amazonaws.com/variants/zw3f2tdkszb7ksbs0de7117jqo4x/4a0f948468b2a28c4c3b6533094ad7d197c89417cdcf6c6140aa5ae9c39cf0a9?response-content-disposition=inline%3B%20filename%3D%22gettyimages-1339908890-2048x20418.jpg%22%3B%20filename%2A%3DUTF-8%27%27gettyimages-1339908890-2048x20418.jpg%26response-content-type=image%2Fjpeg%26X-Amz-Algorithm=AWS4-HMAC-SHA256%26X-Amz-Credential=AKIA2RWTDMGZRLA4VR6P%2F20211128%2Feu-west-1%2Fs3%2Faws4_request%26X-Amz-Date=20211128T032217Z%26X-Amz-Expires=300%26X-Amz-SignedHeaders=host%26X-Amz-Signature=d25dcf1999bf25a7ac29104b5dc8b7d68fba6b177e255ad88549eec84540272c"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="https://params-gg-storage.s3.eu-west-1.amazonaws.com/variants/zw3f2tdkszb7ksbs0de7117jqo4x/4a0f948468b2a28c4c3b6533094ad7d197c89417cdcf6c6140aa5ae9c39cf0a9?response-content-disposition=inline%3B%20filename%3D%22gettyimages-1339908890-2048x20418.jpg%22%3B%20filename%2A%3DUTF-8%27%27gettyimages-1339908890-2048x20418.jpg%26response-content-type=image%2Fjpeg%26X-Amz-Algorithm=AWS4-HMAC-SHA256%26X-Amz-Credential=AKIA2RWTDMGZRLA4VR6P%2F20211128%2Feu-west-1%2Fs3%2Faws4_request%26X-Amz-Date=20211128T032217Z%26X-Amz-Expires=300%26X-Amz-SignedHeaders=host%26X-Amz-Signature=d25dcf1999bf25a7ac29104b5dc8b7d68fba6b177e255ad88549eec84540272c"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from konect.gg/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcUtOIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--1aaa6982f0beefd50ce53d4a6a6046bb83d3d9f6/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9MY21WemFYcGxTU0lNTkRBd2VEUXdNQVk2QmtWVSIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--ae7ea8630d18c3c324709f33325a060a2246c8b9/gettyimages-1339908890-2048x20418.jpg by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 28 Nov 2021 03:22:51 GMT -->
+</HTML>
